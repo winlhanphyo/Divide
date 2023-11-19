42c3c5626051b4d1a942d06c4e5277dbd7ae095c
winlhanphyo <winlhanphyo1@gmail.com>
2023-11-19 13:26:33 +0630

    videoStream API

--- backend/src/services/videoStream/videoStream.service.ts
+++ backend/src/services/videoStream/videoStream.service.ts
@@ -51,14 +51,14 @@ class VideoStreamService {
       var positions = range.replace(/bytes=/, "").split("-");
       var start = parseInt(positions[0], 10);
       var end = positions[1] ? parseInt(positions[1], 10) : total - 1;
-      // var chunksize = (end - start) + 1;
-      // res.writeHead(206, {
-      //   "Content-Range": "bytes " + start + "-" + end + "/" + total,
-      //   "Accept-Ranges": "bytes",
-      //   "Content-Length": chunksize,
-      //   "Content-Type": "video/mp4"
-      // });
-      // res.end(video_file.slice(start, end + 1), "binary");
+      var chunksize = (end - start) + 1;
+      res.writeHead(206, {
+        "Content-Range": "bytes " + start + "-" + end + "/" + total,
+        "Accept-Ranges": "bytes",
+        "Content-Length": chunksize,
+        "Content-Type": "video/mp4"
+      });
+      res.end(video_file.slice(start, end + 1), "binary");
 
       // Create a readable stream from the video file
       const videoStream = fs.createReadStream(path, { start, end });
